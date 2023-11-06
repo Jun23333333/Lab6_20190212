281bee18af955beed8b7fafda37443401a20cedb
weekings <a20190212@pucp.edu.pe>
2023-11-05 21:48:41 -0500

    avence de la preg 2

---
+++ app/src/main/java/com/example/lab6_20190212/foto2.java
@@ -0,0 +1,21 @@
+package com.example.lab6_20190212;
+
+public class foto2 {
+    public foto2(String url) {
+        this.url = url;
+    }
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public foto2(){
+
+    }
+}
--- app/src/main/java/com/example/lab6_20190212/juego23.java
+++ app/src/main/java/com/example/lab6_20190212/juego23.java
@@ -1,5 +1,7 @@
 package com.example.lab6_20190212;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -20,36 +22,42 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class juego23 extends AppCompatActivity {
-    FirebaseFirestore db;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    List<foto> fotitos = new ArrayList<>();
+    List<foto2> fotitos = new ArrayList<>();
 
     String Url1,Url2,Url3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego23);
-        db = FirebaseFirestore.getInstance();
+        Log.d("testeo", "2");
 
         db.collection("segunda").addSnapshotListener((collection, error) -> {
+            Log.d("testeo", "1");
             if (error != null) {
                 Log.d("lectura", "Error listening for document changes.");
                 return;
             }
             if (collection != null && !collection.isEmpty()) {
                 for (QueryDocumentSnapshot document : collection) {
-                    foto foto = document.toObject(foto.class);
+                    foto2 foto = document.toObject(foto2.class);
                     fotitos.add(foto);
-                    Log.d("fotitos", foto.getUrl());
+                    Log.d("testeo", foto.getUrl());
                 }
             }
         });
-        Log.d("testeo","avbs");
         AtomicInteger vol = new AtomicInteger();
         AtomicInteger gan = new AtomicInteger();
-        Url1 = fotitos.get(0).getUrl();
-        Url2 = fotitos.get(1).getUrl();
-        Url3 = fotitos.get(2).getUrl();
+        Log.d("testeo","ae");
+        if (fotitos.size() >= 3) {
+            Url1 = fotitos.get(0).getUrl();
+            Url2 = fotitos.get(1).getUrl();
+            Url3 = fotitos.get(2).getUrl();}else{
+            Url1 = "https://firebasestorage.googleapis.com/v0/b/vertical-reason-402704.appspot.com/o/photos%2FIMG_1acc99da-9c4b-4fa3-b256-9360113d1801.jpg?alt=media&token=1a79f9ab-51ff-4e11-b52d-40d2552b01aa";
+            Url2 = "https://firebasestorage.googleapis.com/v0/b/vertical-reason-402704.appspot.com/o/photos%2FIMG_077c47ce-b84e-4db7-9275-db2ca829397e.jpg?alt=media&token=db2919b8-a435-469e-a200-a7299dc9cefe";
+            Url3 = "https://firebasestorage.googleapis.com/v0/b/vertical-reason-402704.appspot.com/o/photos%2FIMG_61d72850-9a09-4b8c-9a16-9393118c9ffa.jpg?alt=media&token=e7948309-6b8b-461b-9d1b-c5d3edbe04d3";
+        }
         Log.d("testeo","a");
         String Url_f = "https://firebasestorage.googleapis.com/v0/b/vertical-reason-402704.appspot.com/o/Snapshot_1.PNG?alt=media&token=dd44eeb8-1a56-46cd-b75b-9b4a506684b2&_gl=1*19vwuht*_ga*NzE2OTM3NDg2LjE2OTc4NTQxMTY.*_ga_CW55HF8NVT*MTY5OTIzMzAxOS45LjEuMTY5OTIzNDE0MS4xMi4wLjA.";
         ImageView ima1 = findViewById(R.id.ima1);
@@ -66,12 +74,12 @@ public class juego23 extends AppCompatActivity {
         Picasso.get().load(Url_f).into(ima6);
         Log.d("testeo","b");
         List<String> urlsTemporales = new ArrayList<>();
-        urlsTemporales.add(fotitos.get(0).getUrl());
-        urlsTemporales.add(fotitos.get(1).getUrl());
-        urlsTemporales.add(fotitos.get(2).getUrl());
-        urlsTemporales.add(fotitos.get(0).getUrl());
-        urlsTemporales.add(fotitos.get(1).getUrl());
-        urlsTemporales.add(fotitos.get(2).getUrl());
+        urlsTemporales.add(Url1);
+        urlsTemporales.add(Url1);
+        urlsTemporales.add(Url2);
+        urlsTemporales.add(Url2);
+        urlsTemporales.add(Url3);
+        urlsTemporales.add(Url3);
         TextView ganar = findViewById(R.id.textView2);
         Log.d("testeo","c");
         Collections.shuffle(urlsTemporales);
@@ -92,6 +100,11 @@ public class juego23 extends AppCompatActivity {
                 vol.set(0);
             }
             Picasso.get().load(urlsMap.get(0)).into(ima1);
+            Log.d("testeo",urlsMap.get(0));
+            Log.d("testeo",urlsMap.get(1));
+            Log.d("testeo",urlsMap.get(2));
+            Log.d("testeo",urlsMap.get(3));
+            Log.d("testeo",urlsMap.get(4));
             vol.getAndIncrement();
             if(ima1.getDrawable().equals(ima2.getDrawable())){
                 ima1.setVisibility(View.GONE);
